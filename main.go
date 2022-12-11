4fa473b46fbec23cdcad8511cba08ecd64ca9425
Yuya Tajima <happy.cccccc.lemon@gmail.com>
2022-12-11 15:53:42 +0000

    Change main

--- main.go
+++ main.go
@@ -2,55 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/Yuya9786/cube/task"
 	"github.com/Yuya9786/cube/worker"
-	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
 
-func createContainer() (*task.Docker, *task.DockerResult) {
-	c := task.Config{
-		Name:  "test-container-1",
-		Image: "postgres:13",
-		Env: []string{
-			"POSTGRES_USER=cube",
-			"POSTGRES_PASSWORD=secret",
-		},
-	}
-
-	dc, _ := client.NewClientWithOpts(client.FromEnv)
-	d := task.Docker{
-		Client: dc,
-		Config: c,
-	}
-
-	result := d.Run()
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil, nil
-	}
-
-	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
-
-	return &d, &result
-}
-
-func stopContainer(d *task.Docker) *task.DockerResult {
-	result := d.Stop()
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil
-	}
-
-	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
-
-	return &result
-}
-
-func main() {
+func do() {
 	db := make(map[uuid.UUID]*task.Task)
 	w := worker.Worker{
 		Queue: *queue.New(),
@@ -59,7 +20,6 @@ func main() {
 
 	t := task.Task{
 		ID:    uuid.New(),
-		Name:  "test-container-1",
 		State: task.Scheduled,
 		Image: "strm/helloworld-http",
 	}
@@ -76,7 +36,7 @@ func main() {
 
 	fmt.Printf("task %s is running in container %s\n", t.ID, t.ContainerId)
 	fmt.Println("sleepy time")
-	time.Sleep(time.Second * 30)
+	time.Sleep(time.Second * 60)
 
 	fmt.Printf("Stopping task %s\n", t.ID)
 	t.State = task.Completed
@@ -85,4 +45,17 @@ func main() {
 	if result.Error != nil {
 		panic(result.Error)
 	}
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			do()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
