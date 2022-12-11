d5fc5576b56ef7b6f6d7ba53c4451035f27ca5cc
Yuya Tajima <happy.cccccc.lemon@gmail.com>
2022-12-11 23:25:13 +0900

    Add adding and running tasks

--- worker/worker.go
+++ worker/worker.go
@@ -1,6 +1,7 @@
 package worker
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -22,11 +23,44 @@ func (w *Worker) CollectState() {
 	fmt.Println("I will collect stats")
 }
 
-func (w *Worker) RunTask() {
-	fmt.Println("I will start or stop a task")
+func (w *Worker) AddTask(t task.Task) {
+	w.Queue.Enqueue(t)
 }
 
-func (w *Worker) StartTask(t task.Task) {
+func (w *Worker) RunTask() task.DockerResult {
+	t := w.Queue.Dequeue()
+	if t == nil {
+		log.Println("No tasks in the queue")
+		return task.DockerResult{Error: nil}
+	}
+
+	taskQueued := t.(task.Task)
+
+	taskPersisted := w.Db[taskQueued.ID]
+	if taskPersisted == nil {
+		taskPersisted = &taskQueued
+		w.Db[taskQueued.ID] = &taskQueued
+	}
+
+	var result task.DockerResult
+	if task.ValidStateTransition(taskPersisted.State, taskQueued.State) {
+		switch taskQueued.State {
+		case task.Scheduled:
+			result = w.StartTask(taskQueued)
+		case task.Completed:
+			result = w.StopTask(taskQueued)
+		default:
+			result.Error = errors.New("We choud not get here")
+		}
+	} else {
+		err := fmt.Errorf("Invalid transition from %v to %v", taskPersisted.State, taskQueued.State)
+		result.Error = err
+	}
+
+	return result
+}
+
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
 	t.StartTime = time.Now().UTC()
 	config := task.NewConfig(&t)
 	d := task.NewDocker(config)
